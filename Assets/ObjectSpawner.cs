a6da88023d005201c9087806806837305ecd07b4
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-05-26 15:59:41 -0300

    instantiate

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public int freqInSeconds;
+    float nextSpawnTime;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSpawnTime = freqInSeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nextSpawnTime <= Time.time)
+        {
+            Instantiate(prefab);
+            nextSpawnTime += freqInSeconds;
+        }
+    }
+}
